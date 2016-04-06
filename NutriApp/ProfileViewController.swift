775ee4c39a897b03f2886e536999dd6e5eaa5177
Hannah Werbel <hwerbel@uw.edu>
2016-04-05 20:00:37 -0700

    start code for profile

--- NutriApp/ProfileViewController.swift
+++ NutriApp/ProfileViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import Parse
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var goalLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ NutriApp/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  NutriApp
+//
+//  Created by user116674 on 4/5/16.
+//  Copyright © 2016 Kevin Tran. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    class func getUserInfo (id: String) {
+        let predicte = NSPredicate(format: "_id = \(id)")
+        
+    }
+}
