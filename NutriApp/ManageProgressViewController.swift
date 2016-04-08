1db2da8ea7f1c017343cd7cc254f02eeafaf3e3a
Hannah Werbel <hwerbel@uw.edu>
2016-04-07 20:08:46 -0700

    start manage progress

--- NutriApp/ManageProgressViewController.swift
+++ NutriApp/ManageProgressViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Parse
+import ParseUI
 
-class ManageProgressViewController: UIViewController {
-
+class ManageProgressViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var uploadLabel: UILabel!
+    @IBOutlet weak var foodImageView: UIImageView!
+    @IBOutlet weak var uploadView: UIView!
+    let uploadTap = UITapGestureRecognizer()
+    var imageToUpload: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        initialViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +33,61 @@ class ManageProgressViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func initialViews() {
+        //Visual layout
+        foodImageView.image = nil
+        foodImageView.hidden = true
+        uploadView.alpha = 0.8
+        uploadLabel.hidden = false
+        
+        //Tap Gesture Recognizer
+        uploadTap.addTarget(self, action: Selector("onUploadTap:"))
+        uploadView.addGestureRecognizer(uploadTap)
+        uploadView.userInteractionEnabled = true
+    }
+    
+    //Upload image tapped
+    func onUploadTap(recognizer: UITapGestureRecognizer) {
+        //Open image picker
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            //Get image from image picker
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            self.imageToUpload = resize(image, newSize: CGSizeMake(750, 750))
+            
+            //Show image in preview image view
+            self.foodImageView.image = imageToUpload
+            self.foodImageView.hidden = false
+            self.uploadLabel.hidden = true
+                
+            //Dismiss image picker
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+            //Remove tap gesture recognizer from upload view
+            uploadView.removeGestureRecognizer(uploadTap)
+    }
+    
+    //Resize image to fit in Parse's limits
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRectMake(0, 0, newSize.width, newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
 
     /*
     // MARK: - Navigation
